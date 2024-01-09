30f54fcae1948fc80760938c839b2863a11accf9
Nick Drummond <nckdrmmnd@gmail.com>
2024-01-09 09:23:33 -0700

    everything is working as intended

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import user from "../data/user";
+import Links from "/home/freakishdrip/Development/code/se_prep/Phase-2/react-hooks-props-basics-lab/src/components/Links.js";
 
 function About() {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      <p>
+        {user.bio}
+      </p> 
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>
+          Links
+          <br>
+          </br>
+          <br>
+          </br>
+          <Links />  
+      </h3>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+function Links(){
+    return(
+    <div> 
+
+        <ul>
+            <a href="https://github.com/liza" >https://github.com/liza</a>
+        </ul>
+        <ul>
+            <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/ </a>
+        </ul>
+    </div>
+    )
+}
+
+export default Links;
